466a9755dff432923900893753d47e6d09351dd8
Polish  Fuze <33132667+Polishfuze@users.noreply.github.com>
2021-10-24 01:31:41 +0200

    MON
    start id montarzysta task

---
+++ mon/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int n, t, d = 0;
+    cin >> n;
+    cout << "Ok" << n << "\n";
+    int *films;
+    films = (int *)calloc(n * 2, sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        cin >> t;
+        cin >> d;
+        if (d >= t)
+        {
+            films[2 * i] = t;
+            films[2 * i + 1] = d;
+            cout << "OK: " << t << " " << d << "\n";
+        }
+        else
+        {
+            cout << "Impossible to do task!\n";
+        }
+    }
+    cout << "Read ok!\n";
+    for (int i = 0; i < n * 2; i++)
+    {
+        cout << films[i] << "\n";
+    }
+}
